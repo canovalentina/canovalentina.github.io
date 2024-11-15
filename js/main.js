23d07118da720a524f65e9b0cf17326973e79121
canovalentina <vcanoarcay@gmail.com>
2024-11-15 20:00:29 +0100

    added cool new menu right corner

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//menu
+toggle = document.querySelectorAll('.toggle')[0];
+nav = document.querySelectorAll('nav')[0];
+toggle_open_text = 'menu';
+toggle_close_text = 'close';
+toggle.addEventListener(
+  'click',
+  function () {
+    nav.classList.toggle('open');
+    if (nav.classList.contains('open')) {
+      toggle.innerHTML = toggle_close_text;
+    } else {
+      toggle.innerHTML = toggle_open_text;
+    }
+  },
+  false
+);
+setTimeout(function () {
+  nav.classList.toggle('open');
+}, 800);
+//end menu
